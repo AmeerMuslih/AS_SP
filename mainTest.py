646a1a4d3fbc29de71bbadd65c7a0d6e7fd5f5d7
AmeerMuslih <ameer.muslih@outlook.com>
2024-08-19 12:43:06 +0300

    test

--- mainTest.py
+++ mainTest.py
@@ -89,16 +89,17 @@ def main():
 		#for m in range(0,len(CurrentPhoto)):
 		#a= CurrentPhoto[m].InputT
 		#b= CurrentPhoto[m].WeightT 
-		dim1 = dim
-		dim2 = dim//2
+		dim1 = 16
+		dim2 = 8
+		dim3 = 4
 		a = torch.randint(high=50, size=(dim1, dim2))
-		b = torch.randint(high=50, size=(dim2, dim1))
+		b = torch.randint(high=50, size=(dim2, dim3))
 		print(a)
 		print(b)
 		#a=a[:,:,:].detach().cpu()
 		#b=b[:,:].detach().cpu()
 		ref = torch.matmul(a, b)
-		dut, util, cycles, PUs_access_count,AccumulatorBitsCount,Input_A_BitsCount,Input_B_BitsCount,MultiplierToggle,AccumulatorToggle,InputAToggle,InputBToggle = c_smt_sa.exec(a[None,:,:].detach().cpu(),b[:,:].detach().cpu(), dim, 1, 1024)
+		dut, util, cycles, PUs_access_count,AccumulatorBitsCount,Input_A_BitsCount,Input_B_BitsCount,MultiplierToggle,AccumulatorToggle,InputAToggle,InputBToggle = c_smt_sa.exec(a[None,:,:].detach().cpu(),b[:,:].detach().cpu(), dim2, 1, 1024)
 		all_util += PUs_access_count
 		Accumulator_TOT += AccumulatorBitsCount
 		InputA_TOT += Input_A_BitsCount
