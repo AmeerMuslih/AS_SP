c52d4d77f2dcddbe8d0d941a7331b6baecbc0c8b
AmeerMuslih <ameer.muslih@outlook.com>
2024-08-19 10:51:51 +0300

    randint

--- mainTest.py
+++ mainTest.py
@@ -89,7 +89,7 @@ def main():
 		#for m in range(0,len(CurrentPhoto)):
 		#a= CurrentPhoto[m].InputT
 		#b= CurrentPhoto[m].WeightT 
-		a = torch.randn(dim, dim)
+		a = torch.randint(dim, dim)
 		b = torch.ones(dim, dim)
 		print(a)
 		print(b)
